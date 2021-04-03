3215ca669c9ca1afad9b6372db8948af40aa2de3
Dewy <dewy@dewy.dev>
2021-04-03 17:59:29 +0100

    gradle: project setup

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "dev.dewy"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
